4e4ff0b185bcd7289596a03e9e2a83354cece2f4
Pranav Sharma <pranav.sharma.ama@gmail.com>
2022-07-13 18:37:44 +0900

    done with first video!

---
+++ run-prod.sh
@@ -0,0 +1,1 @@
+./scripts/setup.sh && make config=release && ./bin/Release-linux-x86_64/sudoraytracer/sudoraytracer
--- sudoraytracer/src/WalnutApp.cpp
+++ sudoraytracer/src/WalnutApp.cpp
@@ -2,18 +2,67 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
+
+using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		ImGui::Text("Last Render: %.3fms", m_LastRenderTime); 
+		if (ImGui::Button("Render")) {
+            Render();
+        }
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("ViewPort");
+
+        m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+        m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+        if (m_Image) {
+            ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+        }
+
+		ImGui::End();
+        ImGui::PopStyleVar();
+
+        Render();
 	}
+
+    void Render() {
+
+        Timer timer;
+
+        uint32_t buff_len = m_ViewportWidth * m_ViewportHeight;
+
+        if(!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
+            m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+            delete[] m_ImageData;
+
+            m_ImageData = new uint32_t[buff_len];
+
+        }
+
+        for (uint32_t i = 0; i < buff_len; ++i) {
+            m_ImageData[i] = Random::UInt();
+            m_ImageData[i] |= 0xff000000;
+        }
+
+        m_Image->SetData(m_ImageData);
+        m_LastRenderTime = timer.ElapsedMillis();
+    }
+private:
+    std::shared_ptr<Image> m_Image;
+    uint32_t m_ViewportWidth = 0;
+    uint32_t m_ViewportHeight = 0;
+    uint32_t* m_ImageData = nullptr;
+    float m_LastRenderTime = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
